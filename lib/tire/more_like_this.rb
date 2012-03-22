4f26fac430b601d34c1c4c5efdded0099a8af3ae
mereghost <marcello.rocha@gmail.com>
2012-03-22 16:53:36 -0300

    Added support for "more_like_this" queries
    See <http://www.elasticsearch.org/guide/reference/query-dsl/mlt-query.html>
    
    Closes #2.

---
+++ lib/tire/more_like_this.rb
@@ -0,0 +1,43 @@
+# More Like This
+# ==============
+#
+# Author: Mereghost <marcello.rocha@gmail.com>
+#
+#
+# Adds support for "more_like_this" and "more_like_this_field" queries in Tire DSL.
+#
+# It hooks into the Query class and inserts the more_like_this and more_like_this_field query types.
+#
+#
+# Usage:
+# ------
+#
+# Require the component:
+#
+#     require 'tire/more_like_this'
+#
+# From that point on you should have the more_like_this (aliased to mlt) and
+# the more_like_this_field (aliased to mlt_field) queries available.
+#
+#
+# Example:
+# -------
+#
+#     Tire.search 'articles' do
+#       query do
+#          more_like_this 'search for similar text'
+#          more_like_this_field :field_name, 'similar text'
+#       end
+#     end
+#
+# For available options for these queries see:
+#
+# * <http://www.elasticsearch.org/guide/reference/query-dsl/mlt-query.html>
+# * <http://www.elasticsearch.org/guide/reference/query-dsl/mlt-field-query.html>
+#
+#
+require 'tire/more_like_this/more_like_this'
+
+Tire::Search::Query.class_eval do
+  include Tire::Search::MoreLikeThis
+end
