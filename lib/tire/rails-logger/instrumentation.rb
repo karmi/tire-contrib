5df77044edd4aebeec2b3e58d3e355e45e0bdb5a
Oliver Eilhard <oliver.eilhard@gmail.com>
2012-03-24 15:50:40 +0100

    [#3] Fixes: InstanceMethods is deprecated in ActiveSupport::Concern
    Closes [#3]

--- lib/tire/rails-logger/instrumentation.rb
+++ lib/tire/rails-logger/instrumentation.rb
@@ -7,13 +7,11 @@ module Tire
         alias_method_chain :perform, :instrumentation
       end
 
-      module InstanceMethods
-        def perform_with_instrumentation
-          # Wrapper around the Search.perform method that logs search times.
-          #
-          ActiveSupport::Notifications.instrument("search.tire", :name => 'Search', :search => self.to_json) do
-            perform_without_instrumentation
-          end
+      def perform_with_instrumentation
+        # Wrapper around the Search.perform method that logs search times.
+        #
+        ActiveSupport::Notifications.instrument("search.tire", :name => 'Search', :search => self.to_json) do
+          perform_without_instrumentation
         end
       end
     end
