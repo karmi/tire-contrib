72a218fe6cedb28ffb9c2c3f711704b42680ec1d
Timo Schilling <timo@schilling.io>
2013-01-16 03:52:00 +0100

    add namespace/index

---
+++ test/namespace/index_test.rb
@@ -0,0 +1,26 @@
+require 'tire'
+require 'tire/namespace'
+require 'shoulda'
+
+module Tire
+  module Namespace
+    class IndexTest < Test::Unit::TestCase
+      context "Namespace" do
+        context "index" do
+          def teardown
+            Tire::Configuration.reset
+          end
+
+          should "return the name with an namespace" do
+            Tire.configure { namespace "foo" }
+            assert_equal "foo-bar", Tire.index("bar").name
+          end
+
+          should "return the name without an namespace" do
+            assert_equal "bar", Tire.index("bar").name
+          end
+        end
+      end
+    end
+  end
+end
