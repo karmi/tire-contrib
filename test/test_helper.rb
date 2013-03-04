2e9357d63bf8ca9006f7423f7998a9c7ae9dc93c
Dave Kinkead <dave@kinkead.com.au>
2013-03-04 10:10:12 +1000

    Update test_helper and gemspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,22 @@
 require 'rubygems'
-
 require 'pathname'
 require 'test/unit'
-
 require 'shoulda'
 require 'turn' unless ENV["TM_FILEPATH"] || ENV["CI"]
 require 'mocha'
-
+require 'active_support/core_ext/hash/indifferent_access'
 require 'tire'
+
+# => Require models for testing
+require File.dirname(__FILE__) + '/dynamic_persistence/models/validated_model'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_article'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_article_in_index'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_article_in_namespace'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_article_with_casting'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_article_with_defaults'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_articles_with_custom_index_name'
+require File.dirname(__FILE__) + '/dynamic_persistence/models/persistent_articles_with_dynamic_attributes'
+
 
 class Test::Unit::TestCase
 
