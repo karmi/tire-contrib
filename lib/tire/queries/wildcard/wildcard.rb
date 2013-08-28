54c77ffb5d8bc735462f7072bc167f14043a8b3a
Curtis Hatter <curtis.hatter@insightbb.com>
2013-08-27 23:42:51 -0400

    created wildcard query along with tests

---
+++ lib/tire/queries/wildcard/wildcard.rb
@@ -0,0 +1,17 @@
+module Tire
+  module Search
+    module Wildcard
+      def wildcard(field, text, options = {})
+        @value = {:wildcard => { field => { value: text } } }
+        @value[:wildcard][field].update(validate_wildcard_options(options))
+        @value
+      end
+
+      private
+      def validate_wildcard_options(options)
+        valid_options = [:boost]
+        options.delete_if { |key, value| !valid_options.member? key }
+      end
+    end
+  end
+end
---
+++ test/queries/wildcard/wildcard_test.rb
@@ -0,0 +1,30 @@
+require 'tire'
+require 'tire/queries/wildcard'
+require 'shoulda'
+
+module Tire
+  module Search
+    class WildcardTest < Test::Unit::TestCase
+      
+      context "Wildcard queries" do
+        should "search for documents that have fields matching a wildcard expression" do
+          assert_equal({:wildcard => {:user => { value: 'ki*y'}}}, Query.new.wildcard(:user, 'ki*y'))
+        end
+
+        should "allow to boost a wildcard query" do
+          assert_equal({:wildcard => {:user => { value: 'ki*y', :boost => 2.0}}},
+                       Query.new.wildcard(:user, 'ki*y', :boost => 2.0))
+        end
+      end
+      
+      context "validate the options passed" do
+        should "drop all the invalid keys" do
+          assert_equal({:wildcard => {:user => 'ki*y', :boost => 2.0}},
+                       Query.new.mlt(:user, 'ki*y', :boost => 2.0, :foo => :bar))
+          assert_equal({:wildcard => {:user => {:value => 'ki*y', :boost => 2.0}}},
+                       Query.new.mlt_field(:user, 'ki*y', :boost => 2.0, :fields => [:bar]))
+        end
+      end
+    end
+  end
+end
